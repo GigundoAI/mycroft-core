d3bb5945c7a5e71ce4670d57a27775b6044287b3
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-04-02 18:21:26 +0000

    patch feat/tweak_config (#8)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- mycroft/version/__init__.py
+++ mycroft/version/__init__.py
@@ -31,7 +31,7 @@ CORE_VERSION_BUILD = 1
 CORE_VERSION_TUPLE = (CORE_VERSION_MAJOR,
                       CORE_VERSION_MINOR,
                       CORE_VERSION_BUILD)
-CORE_VERSION_STR = '.'.join(map(str, CORE_VERSION_TUPLE))
+CORE_VERSION_STR = '.'.join(map(str, CORE_VERSION_TUPLE)) + "(mycroft-lib)"
 
 
 class VersionManager:
