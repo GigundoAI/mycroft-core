a005c7aa6755f5eb6b71d4cb40846ed4ff29b62e
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-05-05 08:57:24 +0100

    process status tweaks (#30)
    started should be set on start, not after internet connection etc....
    
    in OVOS systemd was restarting skills in a loop because of this
    
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- mycroft/skills/__main__.py
+++ mycroft/skills/__main__.py
@@ -210,6 +210,15 @@ def main(alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
     reset_sigint_handler()
     # Create PID file, prevent multiple instances of this service
     mycroft.lock.Lock('skills')
+
+    callbacks = StatusCallbackMap(on_started=started_hook,
+                                  on_alive=alive_hook,
+                                  on_ready=ready_hook,
+                                  on_error=error_hook,
+                                  on_stopping=stopping_hook)
+    status = ProcessStatus('skills', callback_map=callbacks)
+    status.set_started()
+
     config = Configuration.get()
     # Set the active lang to match the configured one
     set_default_lang(config.get('lang', 'en-us'))
@@ -221,17 +230,12 @@ def main(alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
     bus = start_message_bus_client("SKILLS")
     _register_intent_services(bus)
     event_scheduler = EventScheduler(bus)
-    callbacks = StatusCallbackMap(on_started=started_hook,
-                                  on_alive=alive_hook,
-                                  on_ready=ready_hook,
-                                  on_error=error_hook,
-                                  on_stopping=stopping_hook)
-    status = ProcessStatus('skills', bus, callbacks)
 
     SkillApi.connect_bus(bus)
     skill_manager = _initialize_skill_manager(bus, watchdog)
 
-    status.set_started()
+    status.bind(bus)
+    status.set_alive()
 
     if config["skills"].get("wait_for_internet"):
         _wait_for_internet_connection()
@@ -244,7 +248,6 @@ def main(alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
     skill_manager.start()
     while not skill_manager.is_alive():
         time.sleep(0.1)
-    status.set_alive()
 
     while not skill_manager.is_all_loaded():
         time.sleep(0.1)
